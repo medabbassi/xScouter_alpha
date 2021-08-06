2ee7d7a9292772dcb8ccfe9beb293eb934b54806
Mohamed Abbassi <mabbassix3@gmail.com>
2021-08-06 23:16:08 +0200

    some configs

---
+++ lib/Screens/home/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.amber[600],
+        onTap: _onItemTapped,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {
+                Get.back();
+              },
+              icon: Icon(Icons.sailing),
+              label: Text('back'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/welcome/welcome.dart
+++ lib/Screens/welcome/welcome.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
+import 'package:get/get.dart';
+import 'package:xscouter/Screens/home/home.dart';
 import 'package:xscouter/constants/colors.dart';
 import 'package:xscouter/constants/fonts.dart';
 
@@ -18,7 +21,14 @@ class MyWelcomeScreen extends StatelessWidget {
             Text(
               "hello",
               style: TextStyle(fontFamily: mySecondFont, fontSize: 30),
-            )
+            ),
+            TextButton.icon(
+                onPressed: () {
+                  Get.snackbar('You are using getx', "Good Test");
+                  Get.to(() => HomeScreen());
+                },
+                icon: Icon(Icons.star),
+                label: Text("click me"))
           ],
         ),
       ),
